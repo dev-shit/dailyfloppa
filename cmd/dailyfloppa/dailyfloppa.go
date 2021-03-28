82402845da9291efdbbfdc9497a6adf35027e9cf
Kees van Voorthuizen <me@keesvv.nl>
2021-03-28 19:56:51 +0200

    fix: close connection & quit on signal

--- cmd/dailyfloppa/dailyfloppa.go
+++ cmd/dailyfloppa/dailyfloppa.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/dev-shit/dailyfloppa/internals/bot"
+	"github.com/dev-shit/dailyfloppa/internals/logger"
 )
 
 func main() {
     s := bot.Init()
+    logger.Log("Bot initialized, press CTRL+C to quit")
 
-    bot.SendFloppa(s)
+    // bot.SendFloppa(s)
+    bot.RegisterCommands(s)
 
+    sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+
+	<-sigChan
     s.Close()
 }
