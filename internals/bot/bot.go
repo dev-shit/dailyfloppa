0866151507515e9bc3a3cb14fda84179df4c7383
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-03-28 18:46:09 +0200

    refactor: cleaning up

--- internals/bot/bot.go
+++ internals/bot/bot.go
@@ -5,9 +5,12 @@ import (
 	"os"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/dev-shit/dailyfloppa/internals/logger"
 )
 
 func Init() *discordgo.Session {
+    logger.Log("Initiating connection...")
+
     token, ok := os.LookupEnv("BOT_TOKEN")
     if !ok {
         panic(errors.New("No token provided"))
@@ -20,3 +23,31 @@ func Init() *discordgo.Session {
 
     return s
 }
+
+func SendFloppa(s *discordgo.Session) {
+    channelId, ok := os.LookupEnv("CHANNEL_ID")
+    if !ok {
+        panic(errors.New("No channel ID provided"))
+    }
+
+    _, err := s.ChannelMessageSendComplex(
+        channelId,
+        &discordgo.MessageSend{
+            Embed: &discordgo.MessageEmbed{
+                Title: "Floppa of the day",
+                Image: &discordgo.MessageEmbedImage{
+                    URL: "https://media.tenor.co/videos/a97a96e86b5805aec031c6625b1e13c0/mp4",
+                    Width: 300,
+                    Height: 300,
+                },
+            },
+        },
+    )
+
+    if err != nil {
+        logger.Log(err.Error())
+    }
+
+    logger.Log("Floppa sent")
+}
+
