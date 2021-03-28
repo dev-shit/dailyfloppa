c92fe6e716a33da9296913a9407a586740df73f2
Kees van Voorthuizen <me@keesvv.nl>
2021-03-28 20:14:29 +0200

    feat(commands): log issued commands

--- internals/bot/commands.go
+++ internals/bot/commands.go
@@ -1,9 +1,11 @@
 package bot
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/dev-shit/dailyfloppa/internals/logger"
 )
 
 type Command struct {
@@ -27,7 +29,12 @@ func floppaHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 func RegisterCommands(s *discordgo.Session) {
     s.AddHandler(func(handlerSession *discordgo.Session, m *discordgo.MessageCreate){
         for _, c := range commands {
-            if strings.Split(m.Content, " ")[0] == "f!" + c.Name {
+            cmd := strings.Split(m.Content, " ")[0]
+
+            if cmd == "f!" + c.Name {
+                logger.Log(
+                    fmt.Sprintf("Command '%s' issued by @%s", cmd, m.Author.Username),
+                )
                 c.Handler(s, m)
             }
         }
